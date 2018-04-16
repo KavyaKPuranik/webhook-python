ec6c4d832e0ff476245b197a702b381d7a83baca
KavyaKPuranik <kpuranik08@gmail.com>
2018-04-17 01:09:04 +0530

    Generalizing

--- app.py
+++ app.py
@@ -38,7 +38,11 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-
+	
+	
+    baseurl = "ttps://api.railwayapi.com/v2/"
+    apikey = "/apikey/3gleroll53"
+	
     print("Request:")
     print(json.dumps(req, indent=4))
     if req.get("result").get("action") == "trainStatus":
@@ -59,12 +63,11 @@ def webhook():
 def processCode(req):
     if req.get("result").get("action") != "stationCode":
         return {}
-    baseurl = "ttps://api.railwayapi.com/v2/suggest-station/name/"
-    remain = "/apikey/3gleroll53"
+    remain = "suggest-station/name/"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
-    yql_url = baseurl + yql_query + remain
+    yql_url = baseurl + remain + yql_query + apikey
     result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult4(data)
@@ -72,14 +75,11 @@ def processCode(req):
 def processCode(req):
     if req.get("result").get("action") != "stationCode":
         return {}
-#     baseurl = "https://api.railwayapi.com/v2/name-to-code/station/"
-#     remain = "/apikey/e5hkcdzqsj"
-    baseurl = "https://api.railwayapi.com/v2/suggest-station/name/"
-    remain = "/apikey/3gleroll53"
+    remain = "suggest-station/name/"
     yql_query = makeQueryForPlace(req)
     if yql_query is None:
         return {}
-    yql_url = baseurl + yql_query + remain
+    yql_url = baseurl + remain + yql_query + apikey
     result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult3(data)
@@ -89,12 +89,11 @@ def processCode(req):
 def processRoute(req):
     if req.get("result").get("action") != "trainRoute":
         return {}
-    baseurl = "https://api.railwayapi.com/v2/route/train/"
-    remain = "/apikey/3gleroll53"
+    remain = "route/train/"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
-    yql_url = baseurl + yql_query + remain
+    yql_url = baseurl + remain + yql_query + apikey
     result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult2(data)
@@ -104,17 +103,16 @@ def processRoute(req):
 def processRequest(req):
     if req.get("result").get("action") != "trainStatus":
         return {}
-    baseurl = "https://api.railwayapi.com/v2/live/train/" 
+    remain = "live/train/" 
     i = datetime.datetime.now()
     day = i.day 
     month = i.month 
     year = i.year
     today = "10-04-2018"
-    remain = "/date/"+today+"/apikey/3gleroll53/"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
-    yql_url = baseurl + yql_query + remain
+    yql_url = baseurl + remain + + yql_query + "/date/" + today + apikey
     result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult1(data)
