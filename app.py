aa18bd55cf67b79354de78141f9b1fec16085ea4
KavyaKPuranik <kpuranik08@gmail.com>
2018-04-23 15:52:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -390,13 +390,12 @@ def processSeatAvailability(req):
     data = json.loads(result)   
 #     #Process response
     response_code = data.get("response_code")
-    print(response_code)
     if response_code != 200:
+        print("response code "+response_code)
         speech = "Sorry, No data available. Please check the train number/source/destination/quota/class"
         msg.append(speech)
     else:
         train = json.dumps(data.get("train").get("name"))
-        msg.append(speech)
         speech = "The seat availability for the train " + train
         train_num =  json.dumps(data.get("train").get("number")) 
         print("Here "+train_num)
@@ -404,6 +403,7 @@ def processSeatAvailability(req):
         from_stat = json.dumps(data.get("from_station").get("name")) 
         to_stat = json.dumps(data.get("to_station").get("name")) 
         speech = speech + from_stat + " to the station " + to_stat
+        msg.append(speech)
         print("Speech "+speech)
         availability = []
         for avail in data['availability']:
